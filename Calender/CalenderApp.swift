68f80e17ede4a4a5e7629692a24b4e34e0db40bd
aarnapuvvala <155405226+aarnapuvvala@users.noreply.github.com>
2024-06-23 18:49:42 -0400

    Initial Commit

---
+++ Calender/CalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalenderApp.swift
+//  Calender
+//
+//  Created by Aarna Puvvala on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
